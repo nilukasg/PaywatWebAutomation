b45221d0fb1a66ca573db3d6a40c0d7e590088b4
Niluka Priyadarshani <niluka@Nilukas-MacBook-Pro.local>
2024-04-22 10:29:34 +0530

    Add-Ons settings and Payment flow Assertions

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
 
   reporter: 'cypress-mochawesome-reporter', //for HTML reports
-  
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
